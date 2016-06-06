5432f9d09735265365789dc5b50ae6d218f656e0
muhakh <muhakh2@gmail.com>
2016-06-06 14:33:33 +0200

    This solves #9 Write all logging code for content events

--- plugins/system/userlogs/userlogs.php
+++ plugins/system/userlogs/userlogs.php
@@ -17,6 +17,32 @@ defined('_JEXEC') or die;
 class PlgSystemUserLogs extends JPlugin
 {
     /**
+	 * Function to add logs to the database
+	 * This method adds a record to #__user_logs contains (message, date, context, user)
+	 *
+	 * @param   string   $message  The contents of the message to be logged
+	 * @param   string   $context  The context of the content passed to the plugin
+	 *
+	 * @return  void
+	 *
+	 * @since   3.7
+	 */
+     protected function addLogsToDB($message, $context)
+     {
+         $db = JFactory::getDbo();
+         $user = JFactory::getUser();
+         $date = JFactory::getDate();
+         $query = $db->getQuery(​true​);
+         $columns = array('message', 'log_date', 'extension', 'user_id');
+         $values = array($db->quote($message), $db->quote($date), $db->quote($context), $user->id);
+         $query
+             ->insert($db->quoteName('#__user_logs'))
+             ->columns($db->quoteName($columns))
+             ->values(implode(',', $values));
+         $db->setQuery($query);
+         $db->execute();
+     }
+    /**
 	 * After save content logging method
 	 * This method adds a record to #__user_logs contains (message, date, context, user)
 	 * Method is called right after the content is saved
@@ -32,19 +58,49 @@ class PlgSystemUserLogs extends JPlugin
 	 */
     public function onContentAfterSave($context, $article, $isNew)
     {
-        $db = JFactory::getDbo();
-        $user = JFactory::getUser();
-        $date = JFactory::getDate();
-        $query = $db->getQuery(​true​);
-        $columns = array('message', 'log_date', 'extension', 'user_id');
         $isNew_string = $isNew ? 'true' : 'false';
-        $message = '{"title":"'.$article->title.'","isNew":"'.$isNew_string.'"}';
-        $values = array($db->quote($message), $db->quote($date), $db->quote((string)$context), $user->id);
-        $query
-            ->insert($db->quoteName('#__user_logs'))
-            ->columns($db->quoteName($columns))
-            ->values(implode(',', $values));
-        $db->setQuery($query);
-        $db->execute();
+        $message = '{"title":"'.$article->title.'","isNew":"'.$isNew_string.'", "event":"onContentAfterSave"}';
+        $strContext = (string)$context;
+        $this->addLogsToDB($message, $strContext);
+    }
+
+    /**
+	 * After delete content logging method
+	 * This method adds a record to #__user_logs contains (message, date, context, user)
+	 * Method is called right after the content is deleted
+	 *
+	 * @param   string   $context  The context of the content passed to the plugin
+	 * @param   object   $article  A JTableContent object
+	 *
+	 * @return  void
+	 *
+	 * @since   3.7
+	 */
+    public function onContentAfterDelete($context, $article)
+    {
+        $message = '{"title":"'.$article->title.'","event":"onContentAfterDelete"}';
+        $strContext = (string)$context;
+        $this->addLogsToDB($message, $strContext);
+    }
+
+    /**
+	 * On content change status logging method
+	 * This method adds a record to #__user_logs contains (message, date, context, user)
+	 * Method is called when the status of the article is changed
+	 *
+	 * @param   string   $context  The context of the content passed to the plugin
+	 * @param   array    $pks      An array of primary key ids of the content that has changed state.
+     * @param   int      $value    The value of the state that the content has been changed to.
+	 *
+	 * @return  void
+	 *
+	 * @since   3.7
+	 */
+    public function onContentChangeState($context, $pks, $value)
+    {
+        $message = '{"event":"onContentChangeState",'.
+                    '"primary_keys":'.json_encode($pks).',"value":'.(string)$value.'}';
+        $strContext = (string)$context;
+        $this->addLogsToDB($message, $strContext);
     }
 }
